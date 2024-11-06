f146e8f92bd43fb109f425d63c41d18e2ffd1fda
a21xavmarvel <a21xavmarvel@inspedralbes.cat>
2024-11-06 12:17:09 +0100

    XML FUNCIONAL

--- projecte1.2(final)/app/build.gradle.kts
+++ projecte1.2(final)/app/build.gradle.kts
@@ -50,17 +50,20 @@ android {
 }
 
 dependencies {
+    // Existing dependencies
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("io.socket:socket.io-client:2.0.1")
 
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    implementation ("io.socket:socket.io-client:2.0.1")
+    // Material Design dependency
+    implementation("com.google.android.material:material:1.5.0")
 
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.iid)
     implementation(libs.androidx.recyclerview)
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
